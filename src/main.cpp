4e8a4c8c848697d3097a2f84ade145441d92aaac
astekachev <astekachev@gmail.com>
2022-01-07 16:28:46 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/**************************************************************************************
+ * Put the code from the examples here
+ * Subscribe from ../lib/UAVCAN/examples/UAVCAN-Teensy4/subscribe_hb.cpp
+ * Publish from ../lib/UAVCAN/examples/UAVCAN-Teensy4/publish_hb.cpp
+ * 
+ * Original libraries used in the project
+ * 
+ * CAN Driver: https://github.com/tonton81/FlexCAN_T4
+ * UAVCAN: https://github.com/107-systems/107-Arduino-UAVCAN
+ * 
+ * What has been added to the UAVCAN library:
+ *  ../lib/UAVCAN/src/utility/CritSec-T4.cpp
+ * 
+ * 
+ * WARNING!!!
+ * The project compiles only in the PlatformIO 
+ * 
+ * Configuration on which the code was tested:
+ * 
+ *    OS: MacOS Monterey 12.1
+ *    IDE: Atom 1.58.0 with PlatformIO Core 5.2.4 
+ *    LOADER: Teensy Loader 1.56
+ * 
+ *    PUBLISER: Teensy 4.0 with TJA1051T/3,118
+ *    SUBSCRIBER: Teensy 4.0 with TJA1051T/3,118
+ * 
+ **************************************************************************************/
+
+
