ea9930100f58433d031467c37fe41500f2b082ad
astekachev <astekachev@gmail.com>
2022-01-07 15:49:18 +0300

    Add files via upload

---
+++ lib/UAVCAN/extras/test/include/test/util/Const.h
@@ -0,0 +1,36 @@
+/**
+ * This software is distributed under the terms of the MIT License.
+ * Copyright (c) 2020 LXRobotics.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/107-Arduino-UAVCAN/graphs/contributors.
+ */
+
+#ifndef TEST_UTIL_CONST_H_
+#define TEST_UTIL_CONST_H_
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <ArduinoUAVCAN.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace util
+{
+
+/**************************************************************************************
+ * GLOBAL CONSTANTS
+ **************************************************************************************/
+
+static CanardNodeID const LOCAL_NODE_ID = 13;
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* util */
+
+#endif /* TEST_UTIL_CONST_H_ */
